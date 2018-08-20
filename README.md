0337c9604aed4b4a8aad964efb153cc5dcad50ad
duxiwei <942746803@qq.com>
2018-08-20 15:17:24 +0800

    添加注解

---
+++ README.md
@@ -0,0 +1,2 @@
+### 实现从RN页面跳转到Android原生页面并且传入String值，从Android原生页面跳转到RN页面并且传入String值
+### 详情见:https://github.com/duxiwei/RNAndroid.git
--- android/app/src/main/java/com/rnandroid/NewRNLayout.java
+++ android/app/src/main/java/com/rnandroid/NewRNLayout.java
@@ -3,7 +3,6 @@ package com.rnandroid;
 import com.facebook.react.ReactActivity;
 
 public class NewRNLayout extends ReactActivity {
-
     @Override
     protected String getMainComponentName() {
         return "NewRNLayout";
